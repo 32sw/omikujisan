8e8bdd0ebdfec2b171ae9aa4b31851656e8bc57e
入江翔平 <shoheiirie@irieshoheinombp.lan>
2020-05-13 00:00:23 +0900

    prettier

--- sugoroku.js
+++ sugoroku.js
@@ -1,13 +1,13 @@
-const sugoroku_items = ["一","二","三","四","五","六"];
+const sugoroku_items = ["一", "二", "三", "四", "五", "六"];
 
 const btn = document.getElementById("btn");
 
 const disp = document.getElementById("disp");
 
-const num =0;
+const num = 0;
 
-btn.addEventListener('click',function(){
-    const num = Math.floor(Math.random()*sugoroku_items.length);
+btn.addEventListener('click', function () {
+    const num = Math.floor(Math.random() * sugoroku_items.length);
     disp.innerHTML = sugoroku_items[num];
 
 })
