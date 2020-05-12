798ec67e432a3900ec3f2c022eee2c872601842f
入江翔平 <shoheiirie@irieshoheinombp.lan>
2020-05-12 20:40:59 +0900

    js add

---
+++ sugoroku.js
@@ -0,0 +1,13 @@
+const sugoroku_items = ["一","二","三","四","五","六"];
+
+const btn = document.getElementById("btn");
+
+const disp = document.getElementById("disp");
+
+const num =0;
+
+btn.addEventListener('click',function(){
+    const num = Math.floor(Math.random()*sugoroku_items.length);
+    disp.innerHTML = sugoroku_items[num];
+
+})
